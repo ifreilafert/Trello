9c1171020ec4e945e9d0f50c2d1dfe351e1e74b6
Igor Freilafert <ifreilafert@illumina.com>
2019-05-01 17:58:27 -0700

    Initial commit

---
+++ Trello/ViewModel/HomeViewModel.cs
@@ -0,0 +1,31 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Trello.ViewModel
+{
+    public class HomeViewModel : ViewModelBase
+    {
+        private MainViewModel _mainViewModel;
+        public string ScreenTitle { get; } = "My Title";
+
+        public ICommand NextCommnd { get; private set; }
+
+        public HomeViewModel(MainViewModel mainViewModel)
+        {
+            _mainViewModel = mainViewModel;
+
+            NextCommnd = new RelayCommand(OnNextCommnd);
+        }
+
+        private void OnNextCommnd()
+        {
+
+        }
+    }
+}
